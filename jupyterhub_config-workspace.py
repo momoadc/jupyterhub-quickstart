487af4b1ace28f6706268d251a1a148c28fcdf30
eyal gabizon <eygabi01@gmail.com>
2022-02-06 12:30:34 +0200

    updated the jupyterhub_workspace_config to fit to gitlab

--- jupyterhub_config-workspace.py
+++ jupyterhub_config-workspace.py
@@ -1,154 +1,27 @@
-# Authenticate users against OpenShift OAuth provider.
+from oauthenticator.gitlab import GitLabOAuthenticator
+c.JupyterHub.authenticator_class = GitLabOAuthenticator
+c.GitLabOAuthenticator.oauth_callback_url = 'https://jupyterhub-jupyterhub.apps.lcc570vo.eastus.aroapp.io/hub/oauth_callback'
+c.GitLabOAuthenticator.client_id = 'e39314580b14d1f6d6bf77ae02f21bb140faabda53aa4385c2a145ec8d7eb604'
+c.GitLabOAuthenticator.client_secret = 'f49367ca99eaebe0493f5a204923b998c036cbb497aba095fd355ddac9a8625b'
+c.GitLabOAuthenticator.scope = ['openid']
+c.GitLabOAuthenticator.scope = ['api read_repository write_repository']
 
-c.JupyterHub.authenticator_class = "openshift"
+# async def add_auth_env(spawner):
+#     '''
+#     We set user's id, login and access token on single user image to
+#     enable repository integration for JupyterHub.
+#     See: https://gitlab.com/gitlab-org/gitlab-foss/issues/47138#note_154294790
+#     '''
+#     auth_state = await spawner.user.get_auth_state()
 
-from oauthenticator.openshift import OpenShiftOAuthenticator
-OpenShiftOAuthenticator.scope = ['user:full']
+#     if not auth_state:
+#         spawner.log.warning("No auth state for %s", spawner.user)
+#         return
 
-client_id = '%s-%s-users' % (application_name, namespace)
-client_secret = os.environ['OAUTH_CLIENT_SECRET']
+#     spawner.environment['GITLAB_ACCESS_TOKEN'] = auth_state['access_token']
+#     spawner.environment['GITLAB_USER_LOGIN'] = auth_state['gitlab_user']['username']
+#     spawner.environment['GITLAB_USER_ID'] = str(auth_state['gitlab_user']['id'])
+#     spawner.environment['GITLAB_USER_EMAIL'] = auth_state['gitlab_user']['email']
+#     spawner.environment['GITLAB_USER_NAME'] = auth_state['gitlab_user']['name']
 
-c.OpenShiftOAuthenticator.client_id = client_id
-c.OpenShiftOAuthenticator.client_secret = client_secret
-c.Authenticator.enable_auth_state = True
-
-c.CryptKeeper.keys = [ client_secret.encode('utf-8') ]
-
-c.OpenShiftOAuthenticator.oauth_callback_url = (
-        'https://%s/hub/oauth_callback' % public_hostname)
-
-# Add any additional JupyterHub configuration settings.
-
-c.KubeSpawner.extra_labels = {
-    'spawner': 'workspace',
-    'class': 'session',
-    'user': '{username}'
-}
-
-# Set up list of registered users and any users nominated as admins.
-
-if os.path.exists('/opt/app-root/configs/admin_users.txt'):
-    with open('/opt/app-root/configs/admin_users.txt') as fp:
-        content = fp.read().strip()
-        if content:
-            c.Authenticator.admin_users = set(content.split())
-
-if os.path.exists('/opt/app-root/configs/user_whitelist.txt'):
-    with open('/opt/app-root/configs/user_whitelist.txt') as fp:
-        c.Authenticator.whitelist = set(fp.read().strip().split())
-
-# For workshops we provide each user with a persistent volume so they
-# don't loose their work. This is mounted on /opt/app-root, so we need
-# to copy the contents from the image into the persistent volume the
-# first time using an init container.
-
-volume_size = os.environ.get('JUPYTERHUB_VOLUME_SIZE')
-
-if volume_size:
-    c.KubeSpawner.pvc_name_template = c.KubeSpawner.pod_name_template
-
-    c.KubeSpawner.storage_pvc_ensure = True
-
-    c.KubeSpawner.storage_capacity = volume_size
-
-    c.KubeSpawner.storage_access_modes = ['ReadWriteOnce']
-
-    c.KubeSpawner.volumes.extend([
-        {
-            'name': 'data',
-            'persistentVolumeClaim': {
-                'claimName': c.KubeSpawner.pvc_name_template
-            }
-        }
-    ])
-
-    c.KubeSpawner.volume_mounts.extend([
-        {
-            'name': 'data',
-            'mountPath': '/opt/app-root',
-            'subPath': 'workspace'
-        }
-    ])
-
-    c.KubeSpawner.init_containers.extend([
-        {
-            'name': 'setup-volume',
-            'image': '%s' % c.KubeSpawner.image_spec,
-            'command': [
-                '/opt/app-root/bin/setup-volume.sh',
-                '/opt/app-root',
-                '/mnt/workspace'
-            ],
-            "resources": {
-                "limits": {
-                    "memory": os.environ.get('NOTEBOOK_MEMORY', '128Mi')
-                },
-                "requests": {
-                    "memory": os.environ.get('NOTEBOOK_MEMORY', '128Mi')
-                }
-            },
-            'volumeMounts': [
-                {
-                    'name': 'data',
-                    'mountPath': '/mnt'
-                }
-            ]
-        }
-    ])
-
-# Make modifications to pod based on user and type of session.
-
-from tornado import gen
-
-@gen.coroutine
-def modify_pod_hook(spawner, pod):
-    pod.spec.automount_service_account_token = True
-
-    # Grab the OpenShift user access token from the login state.
-
-    auth_state = yield spawner.user.get_auth_state()
-    access_token = auth_state['access_token']
-
-    # Set the session access token from the OpenShift login.
-
-    pod.spec.containers[0].env.append(
-            dict(name='OPENSHIFT_TOKEN', value=access_token))
-
-    # See if a template for the project name has been specified.
-    # Try expanding the name, substituting the username. If the
-    # result is different then we use it, not if it is the same
-    # which would suggest it isn't unique.
-
-    project = os.environ.get('OPENSHIFT_PROJECT')
-
-    if project:
-        name = project.format(username=spawner.user.name)
-        if name != project:
-            pod.spec.containers[0].env.append(
-                    dict(name='PROJECT_NAMESPACE', value=name))
-
-            # Ensure project is created if it doesn't exist.
-
-            pod.spec.containers[0].env.append(
-                    dict(name='OPENSHIFT_PROJECT', value=name))
-
-    return pod
-
-c.KubeSpawner.modify_pod_hook = modify_pod_hook
-
-# Setup culling of terminal instances if timeout parameter is supplied.
-
-idle_timeout = os.environ.get('JUPYTERHUB_IDLE_TIMEOUT')
-
-if idle_timeout and int(idle_timeout):
-    cull_idle_servers_cmd = ['/opt/app-root/bin/cull-idle-servers']
-
-    cull_idle_servers_cmd.append('--timeout=%s' % idle_timeout)
-
-    c.JupyterHub.services.extend([
-        {
-            'name': 'cull-idle',
-            'admin': True,
-            'command': cull_idle_servers_cmd,
-        }
-    ])
+# c.KubeSpawner.pre_spawn_hook = add_auth_env
